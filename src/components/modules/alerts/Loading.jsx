6d258cf17d9f476181295a917c13f0beaaad2755
DannielMurcia <71535217+murciaue4@users.noreply.github.com>
2025-04-08 18:15:06 -0500

    now yet

--- src/components/modules/alerts/Loading.jsx
+++ src/components/modules/alerts/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-gray-200 bg-opacity-50 fixed top-0 left-0 z-50">
+    <div className="flex justify-center items-center h-screen w-screen bg-background bg-opacity-50 fixed top-0 left-0 z-50">
       <div className="text-center">
         {/* Spinner */}
       
